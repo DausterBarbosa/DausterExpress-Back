8d09038311d3d3a5f57ac5cbce0191cf253d4652
Dauster Barbosa <dausterbarbosa12@gmail.com>
2024-07-21 15:59:54 -0300

    DashboardService configurado

--- src/services/DashboardService.ts
+++ src/services/DashboardService.ts
@@ -7,38 +7,48 @@ import Order from "../entity/Order";
 class DashboardService{
     async list(userId:string){
         const orderRepository = AppDataSource.getRepository(Order);
-
+        
         const pendentes = await orderRepository.count({
             where: {
-                id: userId,
-                status: OrderStatusEnum.pendente,
+                entregador: {
+                    id: userId
+                },
+                status: OrderStatusEnum.pendente
             }
         });
 
         const retiradas = await orderRepository.count({
             where: {
-                id: userId,
+                entregador: {
+                    id: userId
+                },
                 status: OrderStatusEnum.retirado,
             }
         });
 
         const entregues = await orderRepository.count({
             where: {
-                id: userId,
+                entregador: {
+                    id: userId
+                },
                 status: OrderStatusEnum.entregue,
             }
         });
 
         const problemas = await orderRepository.count({
             where: {
-                id: userId,
+                entregador: {
+                    id: userId
+                },
                 status: OrderStatusEnum.problema,
             }
         });
 
         const canceladas = await orderRepository.count({
             where: {
-                id: userId,
+                entregador: {
+                    id: userId
+                },
                 status: OrderStatusEnum.cancelado,
             }
         });
