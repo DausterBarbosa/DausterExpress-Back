9555bbfd53e5055f42ce0dc583e8e187f729c727
Dauster Barbosa <dausterbarbosa12@gmail.com>
2024-08-19 17:05:53 -0300

    pequena alteração na variavel de ambiente

--- src/services/FirebaseService.ts
+++ src/services/FirebaseService.ts
@@ -6,7 +6,7 @@ if (!admin.apps.length) {
     admin.initializeApp({
         credential: admin.credential.cert({
             clientEmail: process.env.CLIENT_EMAIL,
-            privateKey: process.env.PRIVATE_KEY,
+            privateKey: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
             projectId: process.env.PROJECT_ID,
         }),
     });
